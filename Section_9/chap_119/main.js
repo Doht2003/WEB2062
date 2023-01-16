fadde9a7444709bf10b78cbd16b2691671bc389e
Doht2003 <bangngoc535@gmail.com>
2023-01-17 00:13:22 +0700

    119. Summary: Which Data Structure to Use?

---
+++ Section_9/chap_119/main.js
@@ -0,0 +1,19 @@
+// Arrays 
+const arrays = ['Code', 'Eat', 'Code'];
+
+// Sets
+const sets = new Set(['Code', 'Eat', 'Code']);
+
+// Objects
+const objects = {
+    task: 'Code',
+    date: 'today',
+    repeat: true,
+};
+
+// Maps 
+const maps = new Map([
+    ['task', 'Code'],
+    ['date', 'today'],
+    [false, 'Start coding!'],
+]);
