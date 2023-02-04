34683eaeee3b9a8ef30a004858bc6c2d4b5036ef
Doht2003 <bangngoc535@gmail.com>
2023-02-04 23:05:22 +0700

    129. How Passing Arguments Works: Value, Reference

---
+++ Section_10/chap_129/main.js
@@ -0,0 +1,31 @@
+const flight = 'LH234';
+const jonas = {
+    name: 'Jonas Schmedtmann',
+    passport: 570928648736,
+};
+
+const checkIn = function (flightNum, passenger) {
+    flightNum = 'LH999';
+    passenger.name = 'Mr. ' + passenger.name;
+
+    if (passenger.passport === 570928648736) {
+        alert('Check in');
+    } else {
+        alert('Wrong passport');
+    }
+};
+
+checkIn(flight, jonas);
+console.log(flight);
+console.log(jonas);
+
+// Is the same as doing...
+// const flightNum = flight;
+// const passenger = jonas;
+
+const newPassport = function (person) {
+    person.passport = Math.trunc(Math.random() * 100000000000);
+};
+newPassport(jonas);
+checkIn(flight, jonas);
+
