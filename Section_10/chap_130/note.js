cffdbc800681eb241851c9517567dd21da935f18
Doht2003 <bangngoc535@gmail.com>
2023-02-04 23:13:23 +0700

    130. First-Class and Higher-Order Functions

---
+++ Section_10/chap_130/note.js
@@ -0,0 +1,14 @@
+//first class
+//1.first class functions this means that functions are treated as values they are another type of object
+ 
+//2.we can sotre functions in variables or properties
+//passing functions as argumetns to OTHER functions an example can be is a event listener
+ 
+//3.they can return functions FROM other functions
+ 
+//4.call methods on fuctions bind method
+ 
+//higher order
+//1. A function that receives another function as an argumrnt that returns a new function or both this is only possible because of first class functions
+ 
+//a event listener is a higher order function becasue it receives a call back function its called a call back function because it will be called later by the higher order function
