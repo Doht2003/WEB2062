9c59b9ddeef3156a942c707bbd70912be3a5c7ea
Doht2003 <bangngoc535@gmail.com>
2023-02-06 12:50:26 +0700

    143. The new at Method

---
+++ Section_11/chap_143/main.js
@@ -0,0 +1,11 @@
+const arr = [23, 11, 64];
+console.log(arr[0]);
+console.log(arr.at(0));
+
+// getting last array element
+console.log(arr[arr.length - 1]);
+console.log(arr.slice(-1)[0]);
+console.log(arr.at(-1));
+
+console.log('joans'.at(0));
+console.log('joans'.at(-1));
